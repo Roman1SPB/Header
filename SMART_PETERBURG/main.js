d4f4647d7e215444bf327bcce6b80c9db422bff7
Roman1SPB <122105325+Roman1SPB@users.noreply.github.com>
2024-01-13 14:30:35 +0300

    Add files via upload

---
+++ SMART_PETERBURG/main.js
@@ -0,0 +1,14 @@
+function show_login_form() {
+    document.getElementById('container').style.display = "block";
+ }
+ const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
